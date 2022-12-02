93b0bfc5d51aba9f8a28458d7969ce6d00ce2a3b
KevinModernel <modernelkevin96@gmail.com>
2022-12-02 12:39:12 -0300

    Implement critical stock route
    Add route, controller and view, to show products with less than 5 in stock, and restock if it is wanted by the admin.

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -14,9 +14,18 @@ const updateStatus = async (req, res) => {
 
 const showCritical = async (req, res) => {
 	const criticalStock = await Products.find({ quantity: { $lt: 5} });
-	console.log("CriticalStock: " + criticalStock);
-	res.render('critical');
+	res.render('critical', { criticalStock });
 };
 
+const restock = async (req, res) => {
+	const restock = Number(req.body.restock);
+	const idProduct = req.params.id;
+	const product = await Products.findOne({ _id: idProduct});
+	const currentStockProduct = product.quantity;
+	const actualStock = currentStockProduct + restock;
+	await Products.updateOne({ _id: idProduct}, { quantity: actualStock});
+	res.redirect('/admin/critical');
+}
 
-module.exports = { show_orders, updateStatus, showCritical }
+
+module.exports = { show_orders, updateStatus, showCritical, restock }
--- src/routes/admin_routes.js
+++ src/routes/admin_routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { addProduct, show_add_product } = require('../controllers/products-controller.js');
-const { show_orders, updateStatus, showCritical } = require('../controllers/admin-controller.js');
+const { show_orders, updateStatus, showCritical, restock } = require('../controllers/admin-controller.js');
 
 // Routes /admin
 router.get('/addproduct', show_add_product);
@@ -9,6 +9,7 @@ router.post('/addproduct', addProduct);
 router.get('/orders', show_orders);
 router.put('/orders/:id', updateStatus);
 router.get('/critical', showCritical)
+router.post('/restock/:id', restock)
 
 
 module.exports = router
