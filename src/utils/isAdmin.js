535056815f20e6e800e1c1aa9b2622f4b0e08d70
KevinModernel <modernelkevin96@gmail.com>
2022-12-05 15:39:56 -0300

    Implement isAdmin middleware
    Middleware to allow or deny access wheter is an admin or not

---
+++ src/utils/isAdmin.js
@@ -0,0 +1,16 @@
+const isAdmin = ( req, res, next ) => {
+    console.log("req.user: " + req.user);
+    if (req.user) { // Si esta logeado, se fija si es admin.
+    	const isAdmin = req.user.isAdmin;
+    	if (isAdmin == 'true') { // Si es admin, te permite ir a rutas de admin.
+    		return next();
+    	} else { // Si no es admin, 404.
+    		res.redirect('/notfound');
+    	};
+    } else{
+    	res.redirect('/notfound');
+    };
+    
+}
+
+module.exports = { isAdmin }
